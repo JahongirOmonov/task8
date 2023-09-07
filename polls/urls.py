90c8f7a8c5532f01b82cc06f928a4d7ef316ec0d
Jahongir <dodajoy0777@gmail.com>
2023-09-07 07:29:02 +0500

    hh

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from .models import naushnik, allinfo
-from .views import all, detail
+from .views import getallinfo, postallinfo, detail
 
 urlpatterns=[
-    path('all/', all),
-    path('detail/<int:myid>', detail)
+    path('all/', getallinfo.as_view()),
+    path('detail/<int:myid>', detail),
+    path('create/', postallinfo.as_view())
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,18 +3,26 @@ from django.shortcuts import render, get_object_or_404
 from .models import allinfo, naushnik
 from django.http import JsonResponse
 from .serializer import allinfoSerializer, naushnikSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-def all(request):
-    all=allinfo.objects.all()
-    result=allinfoSerializer(all, many=True)
-    # for i in hammasi:
-    #     natija.append({
-    #         'name':i.name,
-    #         'count':i.count
-    #     })
-    return JsonResponse(result.data, safe=False)
+# def all(request):
+#     all=allinfo.objects.all()
+#     result=allinfoSerializer(all, many=True)
+#     # for i in hammasi:
+#     #     natija.append({
+#     #         'name':i.name,
+#     #         'count':i.count
+#     #     })
+#     return JsonResponse(result.data, safe=False)
+
+class getallinfo(APIView):
+    def get(self, request):
+        nega = allinfo.objects.all()
+        shunga=allinfoSerializer(nega, many=True)
+        return Response(shunga.data)
     
 
 
@@ -24,4 +32,13 @@ def detail(request, myid):
     some = naushnik.objects.filter(id=myid)
     forgett = naushnikSerializer(some, many=True)
     return JsonResponse(forgett.data, safe=False)
+
+class postallinfo(APIView):
+    def post(self, request):
+        goodd=request.data
+        duck = allinfoSerializer(data=goodd)
+        if duck.is_valid():
+            duck.save()
+            return Response(duck.data)
+        return Response(duck.errors)
         
