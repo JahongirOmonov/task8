602ddf620b0d47f65a86cff4e77cc8a3d570424a
Jahongir <dodajoy0777@gmail.com>
2023-09-16 07:51:39 +0500

    last

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,94 @@
+from .views import GetAllNaushnik, GetDetailNaushnik, PostNaushnik, PatchNaushnik, DeleteNaushnik, AllFunctionNaushnik, PostGetNaushnik
 from django.urls import path
-# from .models import naushnik, allinfo
-from .views import getallinfo, postallinfo, detail
+
+
+
+
+
+
+
+
+
+from .views import GetAllAllinfo, PostAllinfo, GetDetailAllinfo, PatchAllinfo, DeleteAllinfo, AllFunctionAllinfo, PostGetAllinfo
 
 urlpatterns=[
-    path('all/', getallinfo.as_view()),
-    path('detail/<int:myid>', detail),
-    path('create/', postallinfo.as_view())
+    path('GetAllAllinfo/', GetAllAllinfo.as_view()),
+
+
+
+
+
+
+
+    path('GetDetailAllinfo/<int:pk>', GetDetailAllinfo.as_view()),
+
+
+
+
+
+
+
+    path('PostAllinfo/', PostAllinfo.as_view() ),
+
+
+
+
+
+
+
+    path('PatchAllinfo/<int:pk>', PatchAllinfo.as_view()),
+    path('DeleteAllinfo/<int:pk>', DeleteAllinfo.as_view()),
+
+
+
+
+
+
+
+    path('PostGetAllinfo/', PostGetAllinfo.as_view()),
+    path('AllFunctionAllinfo/<int:pk>', AllFunctionAllinfo.as_view()),
+
+
+
+
+
+
+
+    path('GetAllNaushnik/', GetAllNaushnik.as_view()),
+    path('GetDetailNaushnik/<int:pk>', GetDetailNaushnik.as_view()),
+
+
+
+
+
+
+
+    path('PostNaushnik/', PostNaushnik.as_view() ),
+    path('PatchNaushnik/<int:pk>', PatchNaushnik.as_view()),
+
+
+
+
+
+
+
+    path('DeleteNaushnik/<int:pk>', DeleteNaushnik.as_view()),
+
+
+
+
+
+
+
+    path('PostGetNaushnik/', PostGetNaushnik.as_view()),
+    path('AllFunctionNaushnik/<int:pk>', AllFunctionNaushnik.as_view())
+
+
+
+
+
+
+
+
+
 ]
